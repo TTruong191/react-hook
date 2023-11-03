96ff863e33767fd912f454e32b8580ad8b20e224
totruong <totruong591@gmail.com>
2023-11-03 16:21:46 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
 function App() {
-  const name = 'Tobie';
-  const obj = { name: 'Obito', age: '25' }
+  // const name = 'Tobie';
+  // const obj = { name: 'Obito', age: '25' }
+  const [name, setName] = useState('Tobie') 
+  const [address, setAddress] = useState('')
+
+  const handleEventClick = (event) => {
+    setName(address)
+    console.log(address)
+  }
+  const handleOnchangeInput = (event) => {
+    setAddress(event.target.value)
+
+  }
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with  React and name {name}</h1>
-        <p>{JSON.stringify(obj)}</p>
-        <button type='button'>Click me</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <p>{JSON.stringify(obj)}</p> */}
+
+        <input type="text" value={address} onChange={(event) => handleOnchangeInput(event)}  />
+        <button type='button' onClick={(event) => handleEventClick(event)}>Click me</button>
+        
       </header>
     </div>
   );
