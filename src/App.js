0b78971853978a9a3bc5586c977a32692fc4176f
totruong <totruong591@gmail.com>
2023-11-04 09:53:08 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 import { useState } from 'react';
+import Todo from './views/Todo';
+import { type } from '@testing-library/user-event/dist/type';
 
 function App() {
   // const name = 'Tobie';
   // const obj = { name: 'Obito', age: '25' }
-  const [name, setName] = useState('Tobie') 
+  const [name, setName] = useState('Tobie')
   const [address, setAddress] = useState('')
   const [todos, setTodo] = useState([
-    { id: 'todo1', title: 'Chơi game' },
-    { id: 'todo2', title: 'Đá bóng' },
-    { id: 'todo3', title: 'Hít đất' }
+    { id: 'todo1', title: 'Chơi game', type: 'A1' },
+    { id: 'todo2', title: 'Đá bóng', type: 'A1' },
+    { id: 'todo3', title: 'Hít đất', type: 'A2' },
+    { id: 'todo4', title: 'Bóng chuyền', type: 'A2' },
   ])
 
   const handleEventClick = (event) => {
-    if(!address) {
+    if (!address) {
       alert('empty title')
       return;
-      }
-  const todo = { id: 'todo4', title: address}
-   setTodo([...todos, todo])
-   setAddress('')
+    }
+    const todo = { id: 'todo4', title: address }
+    setTodo([...todos, todo])
+    setAddress('')
   }
   const handleOnchangeInput = (event) => {
     setAddress(event.target.value)
@@ -35,18 +38,19 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with  React and name {name}</h1>
         {/* <p>{JSON.stringify(obj)}</p> */}
-        <div className='todo-container'>
-          {todos.map(todo => {
-            return(
-            <li className='todo-child' key={todo.id}>{todo.title}</li>
-            )
-          })}
-          
-        </div>
+        <Todo
+          myData={todos}
+          title="All todo"
+        />
 
-        <input type="text" value={address} onChange={(event) => handleOnchangeInput(event)}  />
+        <Todo
+          myData={todos.filter(item => item.type === "A1")}
+          title="Type todo"
+        />
+
+        <input type="text" value={address} onChange={(event) => handleOnchangeInput(event)} />
         <button type='button' onClick={(event) => handleEventClick(event)}>Click me</button>
-        
+
       </header>
     </div>
   );
---
+++ src/views/Todo.js
@@ -0,0 +1,21 @@
+
+
+function Todo(props) {
+  const todos = props.myData;
+  return (
+
+    <div className='todo-container'>
+      <div className="title">
+          {props.title}
+      </div>
+      {todos.map(todo => {
+        return (
+          <li className='todo-child' key={todo.id}>{todo.title}</li>
+        )
+      })}
+      <hr/>
+    </div>
+  )
+}
+
+export default Todo;
